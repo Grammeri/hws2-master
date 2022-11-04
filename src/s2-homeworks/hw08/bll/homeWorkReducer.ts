185e77969c04333b6030080a6f07bdc84765dad5
Grammeri <reacttsxgrammer@gmail.com>
2022-11-04 10:49:31 -0500

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,23 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: any): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            return [...state, action.payload.up ? state.sort(): state.reverse()]
+            const stateCopy = [...state];
+            stateCopy.sort((a, b) => {
+                if (action.payload === "up") {
+                    return a.name > b.name ? 1 : -1
+                } else {
+                    return a.name < b.name ? 1 : -1
+                }
+            })
+            return stateCopy
 
         }
         case 'check': {
 
-           return state.filter((f:UserType) => f.age > action.payload ) // need to fix
+            return state.filter((f: UserType) => f.age > action.payload) // need to fix
         }
         default:
             return state
