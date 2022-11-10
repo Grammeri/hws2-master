746fc6af91a253c127b8dfa8df2cd3215708e8ae
Grammeri <reacttsxgrammer@gmail.com>
2022-11-10 08:56:36 -0600

    hw12

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -4,6 +4,7 @@ import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
 import {useDispatch, useSelector} from 'react-redux'
 import {changeThemeId} from './bll/themeReducer'
+import {AppStoreType} from "../hw10/bll/store";
 
 /*
 * 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
@@ -18,12 +19,21 @@ const themes = [
     {id: 3, value: 'dark'},
 ]
 
+/*type themesType = {
+    id:number
+    value: "light" | "blue" | "dark"
+}*/
+
 const HW12 = () => {
     // взять ид темы из редакса
-    const themeId = 1
 
-    const change = (id: any) => { // дописать функцию
+    const dispatch = useDispatch()
+    const themeId = useSelector<AppStoreType, any>(state => state.theme.themeId)
 
+    /*const themeId = 1*/
+
+    const change = (id: number) => { // дописать функцию
+        dispatch(changeThemeId(themeId))
     }
 
     useEffect(() => {
@@ -41,7 +51,8 @@ const HW12 = () => {
                     id={'hw12-select-theme'}
                     className={s.select}
                     // сделать переключение тем
-
+                    /*onChangeOption={(e)=>{change(e.currentTarget.value)}}*/
+                    onChangeOption={change}
                 />
             </div>
         </div>
