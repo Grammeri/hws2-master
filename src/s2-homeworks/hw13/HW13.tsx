0ee5fe638a312c50f004dfeb39d90824910e70e8
Grammeri <reacttsxgrammer@gmail.com>
2022-11-21 16:19:17 -0600

    HW-13

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -70,7 +70,7 @@ const HW13 = () => {
                         onClick={send(false)}
                         xType={'secondary'}
                         // дописать
-
+                        disabled={info === "...loading"}
                     >
                         Send false
                     </SuperButton>
